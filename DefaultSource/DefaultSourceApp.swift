89336f420e2838c03cc97e1020187b06c3f173a5
gp <tranhrp@gmail.com>
2024-08-16 23:36:54 +0700

    Initial Commit

---
+++ DefaultSource/DefaultSourceApp.swift
@@ -0,0 +1,32 @@
+//
+//  DefaultSourceApp.swift
+//  DefaultSource
+//
+//  Created by Giang Phạm on 16/08/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DefaultSourceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
