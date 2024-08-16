863d697372a79257ac9bd8f460f45eb664d6a095
gp <tranhrp@gmail.com>
2024-08-17 02:17:12 +0700

    first commit

---
+++ DefaultSource/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  DefaultSource
+//
+//  Created by Giang Phạm on 16/08/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
